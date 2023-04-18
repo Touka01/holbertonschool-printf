339c3622a369040de94bc772b1b55356fc968d44
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-04-18 16:09:14 +0100

    function

--- _printf.c
+++ _printf.c
@@ -1,53 +1,47 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
+
+/**
+ *_printf - Customized printf function
+ *@format: constant char
+ *Return: Number of characters printed
+ */
 
 int _printf(const char *format, ...)
 {
-	int chars = 0;
-	va_list args;
-	va_start(args, format);
+	int count = 0;
 
+	va_list args;
+
+	va_start(args, format);
+	if (!format || *format == '\0')
+		return (-1);
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-			if (*format == 'c')
-			{
-				int c = va_arg(args, int);
-				putchar(c);
-				chars++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-				while (*s)
-				{
-					putchar(*s);
-					s++;
-					chars++;
-				}
-			}
-			else if (*format == '%')
+			if (*format == '%')
 			{
 				putchar('%');
-				chars++;
+				count++;
+			}
+			else if (*format == '\0')
+			{
+				va_end(args);
+				return (-1);
 			}
 			else
 			{
-				putchar('%');
-				putchar(*format);
-				chars += 2;
+				count += get_format_specifier(*format, args);
 			}
 		}
 		else
 		{
 			putchar(*format);
-			chars++;
+			count++;
 		}
 		format++;
 	}
-
 	va_end(args);
-	return chars;
+	return (count);
 }
