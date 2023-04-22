5323369a7ba6b0c8be945eb9630245784ba9d21f
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-04-22 17:11:31 +0100

    about all the function specifers

--- _printf.c
+++ _printf.c
@@ -1,46 +1,49 @@
 #include "main.h"
-
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
 /**
- *_printf - Customized printf function
- *@format: constant char
- *Return: Number of characters printed
+ * _printf - function like printf
+ * @format: the pointer of char
+ * Return: 1
  */
-
 int _printf(const char *format, ...)
 {
-	int count = 0;
-
+	int count = 0, f = 0, i = 0;
 	va_list args;
 
-	va_start(args, format);
-	if (!format || *format == '\0')
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	while (*format)
+	va_start(args, format);
+	while (format[i])
 	{
-		if (*format == '%')
+		if (format[i] != '%')
 		{
-			format++;
-			if (*format == '%')
-			{
-				putchar('%');
-				count++;
-			}
-			else if (*format == '\0')
-			{
-				va_end(args);
-				return (-1);
-			}
-			else
-			{
-				count += get_format_specifier(*format, args);
-			}
-		}
-		else
-		{
-			putchar(*format);
+			putchar(*(format + i));
 			count++;
 		}
-		format++;
+		if (format[i] == '%')
+		{
+			f = get_function(format[i + 1], args);
+			if (f != 0)
+			{
+				count = count + f;
+				i = i + 2;
+				continue;
+			}
+			if (format[i] == '\0')
+			{
+				putchar(format[i]);
+				count++;
+			}
+			else if ((format[i] == '%' && format[i + 1] == 'K') ||
+					 (format[i] == '%' && format[i + 1] == '!'))
+			{
+				putchar(format[i]);
+				count++;
+			}
+		}
+		i++;
 	}
 	va_end(args);
 	return (count);
