fb8f8df5ebc4bc99c0e0b9c4891bb6b8e4238bb6
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-04-22 17:20:53 +0100

    header fils of the function _printf

--- main.h
+++ main.h
@@ -1,29 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdarg.h>
-/**
- *struct format - data structure for the printf
- *@ch: char representing the format specifier character
- *@f: function pointer to the corresponding print function for the specifier
- */
-
-typedef struct format
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+typedef struct print
 {
-	char ch;
+	char c;
 	int (*f)(va_list);
-} format_specifier;
-
-int get_format_specifier(const char c, va_list args);
+} print_t;
 int _printf(const char *format, ...);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-int print_int(va_list args);
-int print_int_helper(int n);
-int main(void);
+int get_function(const char s, va_list args);
+
+int printint(va_list arg);
+int printpercent(va_list arg);
+int printstring(va_list arg);
+int printcharacter(va_list arg);
 
 #endif
